45ef3f4ac17f10a0350162aaedd55de9f1f533b5
Daniel Arqués Toro <dani.arques11@gmail.com>
2024-12-01 19:24:09 +0100

    refactor: Fix NotFoundPage.tsx style

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,19 +1,19 @@
-import { Link } from 'react-router-dom';
-import { FaExclamationTriangle } from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+import {FaExclamationTriangle} from 'react-icons/fa';
 
 const NotFoundPage = () => {
-  return (
-    <section className='text-center flex flex-col justify-center items-center h-96'>
-      <FaExclamationTriangle className='text-yellow-400 text-6xl mb-4' />
-      <h1 className='text-6xl font-bold mb-4'>404 Not Found</h1>
-      <p className='text-xl mb-5'>This page does not exist</p>
-      <Link
-        to='/'
-        className='text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4'
-      >
-        Go Back
-      </Link>
-    </section>
-  );
+    return (
+        <section className='text-center flex flex-col justify-center items-center m-5'>
+            <FaExclamationTriangle className='text-warning mb-4' size={50}/>
+            <h1 className='font-bold mb-4'>404 Not Found</h1>
+            <h4 className='mb-5'>This page does not exist</h4>
+            <Link
+                to='/'
+                className='text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4'
+            >
+                Go Back
+            </Link>
+        </section>
+    );
 };
 export default NotFoundPage;
